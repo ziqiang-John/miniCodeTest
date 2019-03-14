e86580c176e3d87ea63f5f2ff6eef01738828d4f
ziqiang.a.huang <haimsn@126.com>
2019-03-14 14:21:38 +0800

    mini coding test.

--- src/main/java/MiniCodeTestStage1.java
+++ src/main/java/MiniCodeTestStage1.java
@@ -1,17 +1,17 @@
 public class MiniCodeTestStage1 {
     public static void main(String[] args){
         for(int i = 1;i<=100;i++){
-            if(i%15==0){
-                System.out.println("FizzBuzz");
-            }else {
-                if (i % 3 == 0) {
-                    System.out.println("Fizz");
-                } else if (i % 5 == 0) {
-                    System.out.println("Buzz");
-                } else {
-                    System.out.println(i);
+            if (i % 3 == 0) {
+                System.out.print("Fizz");
+                if(i%5 == 0){
+                    System.out.print("Buzz");
                 }
+            }else if (i % 5 == 0) {
+                System.out.print("Buzz");
+            }else {
+                System.out.print(i);
             }
+            System.out.println();
         }
     }
 }
--- src/main/java/MiniCodeTestStage2.java
+++ src/main/java/MiniCodeTestStage2.java
@@ -1,9 +1,9 @@
 public class MiniCodeTestStage2 {
     public static void main(String[] args){
-        for(int i=1;i<=100;i++){
-            if(i%3==0||i/10==3||i%10==3){
+        for(int i= 1;i <= 100;i++){
+            if(i%3 == 0||i/10 == 3||i%10 == 3){
                 System.out.print("Fizz");
-                if(i%5==0||i/10==5||i%10==5){
+                if(i%5 == 0||i/10 == 5||i%10 == 5){
                     System.out.print("Buzz");
                 }
             }else if(i%5 == 0||i/10 == 5||i%10 == 5){
