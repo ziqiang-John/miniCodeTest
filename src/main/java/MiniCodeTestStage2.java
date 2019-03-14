a3dbda2941cf9058a7bc010210e09aa580a4f71b
ziqiang.a.huang <haimsn@126.com>
2019-03-14 11:01:12 +0800

    mini coding test.

---
+++ src/main/java/MiniCodeTestStage2.java
@@ -0,0 +1,18 @@
+public class MiniCodeTestStage2 {
+    public static void main(String[] args){
+        for(int i=1;i<=100;i++){
+            if(i%3==0||i/10==3||i%10==3){
+                System.out.print("Fizz");
+                if(i%5==0||i/10==5||i%10==5){
+                    System.out.print("Buzz");
+                }
+            }else if(i%5 == 0||i/10 == 5||i%10 == 5){
+                System.out.print("Buzz");
+            }else{
+                System.out.print(i);
+            }
+            System.out.println();
+
+        }
+    }
+}
